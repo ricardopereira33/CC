ad66ac61e2cd6356c3f3ccbfa9a87dbb24553783
Ricardo Pereira <ricardo.agp.914@gmail.com>
2017-10-29 10:51:32 +0000

    small fixes

--- Server/Monitor.java
+++ Server/Monitor.java
@@ -9,7 +9,7 @@ public class Monitor extends Thread{
 	private ServerStatus ss;
 	private int time;
 	private MonitorThread mt;
-	
+
 	public Monitor(ServerStatus ss, int time,String address){
 		this.port = 5555;
 		this.ss = ss;
@@ -18,18 +18,18 @@ public class Monitor extends Thread{
 	}
 
 	public void run (){
-		
+
 		byte[] buffer = new byte[512];
 		DatagramPacket packet = new DatagramPacket(buffer,buffer.length);
 		int i =0;
 		try{
 			addr = InetAddress.getByName(this.address);
 			this.socket = new DatagramSocket(port);
-			System.out.println("Monitor stared");
-			
+			System.out.println("Monitor stared!!");
+
 			//Thread que envia pacotes do tipo "Available"
 			this.mt = new MonitorThread(ss,time,socket,this.address);
-			
+
 			mt.start();
 
 			while(true){
@@ -37,18 +37,15 @@ public class Monitor extends Thread{
 				this.socket.receive(packet);
 				PacoteMonitor pm = new PacoteMonitor(packet.getData());
 
-				//pm.print();
-				
 				String msg = "Probing";
 
 				//enviar pacoteMonitor
 				PacoteMonitor pm2 = new PacoteMonitor(pm.getNumPacote(),msg,packet.getAddress(),packet.getPort());
 				pm2.setTempSaida(pm.getTempSaida());
 				byte[] buf = pm2.converteByte();
-				
+
 				DatagramPacket out = new DatagramPacket(buf, buf.length, packet.getAddress(),packet.getPort());
 				this.socket.send(out);
-				//System.out.println(info);
 			}
 		}
 		catch(Exception e){
